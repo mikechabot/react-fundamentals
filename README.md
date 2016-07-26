b73140cd7839fde24305c4ec11aeaebdfd684cff
Mike Chabot <mike.chabot@live.com>
2016-07-25 22:04:37 -0400

    Adding lesson 9

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 # react-fundamentals
 React fundamentals class for BU's CS department
 
-## Lesson 8
+## Lesson 9
 
-Example of adding dynamic elements to a rendered array.
+Example of dynamically popping from a rendered array.
 
 ### Get Started
 1. git clone https://github.com/mikechabot/react-fundamentals.git
--- src/root.jsx
+++ src/root.jsx
@@ -8,6 +8,7 @@ export default class Root extends React.Component {
             names: ['Foo', 'Bar', 'Baz']
         };
         this._addToList = this._addToList.bind(this);
+        this._removeFromList = this._removeFromList.bind(this);
     }
 
     _addToList() {
@@ -16,11 +17,18 @@ export default class Root extends React.Component {
         this.setState({ names });
     }
 
+    _removeFromList() {
+        const { names } = this.state;
+        names.pop();
+        this.setState({ names });
+    }
+
     render() {
         const { names } = this.state;
         return (
             <div>
                 <button onClick={this._addToList}>Add Random Number</button>
+                <button onClick={this._removeFromList}>Remove From List</button>
                 <ol>
                     {
                         names.map((name, index) => (
